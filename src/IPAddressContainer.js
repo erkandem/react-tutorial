e366347b80db1594b93ca38c31ae0796d5ad5ebe
kan <erkan.dem@pm.me>
2020-05-03 14:37:46 +0200

    basic code to execute remote call, process response and render result

--- src/IPAddressContainer.js
+++ src/IPAddressContainer.js
@@ -1,8 +1,49 @@
 import React from "react"
+var xhr;
 
 class IPAddressContainer extends React.Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        ip_address: "..."
+    };
+    this.processRequest = this.processRequest.bind(this);
+    }
+
+    componentDidMount(){
+        /*
+        access from `http://localhost:<port>/` won't work in firefox.
+        Try the local network address instead.
+        reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSDidNotSucceed?utm_source=devtools&utm_medium=firefox-cors-errors&utm_campaign=default
+
+        reference: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        */
+        xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://ipinfo.io/json", true);
+        xhr.send();
+        xhr.addEventListener("readystatechange", this.processRequest, false);
+        // or
+        // xhr.onreadystatechange = this.processRequest
+    }
+
+    processRequest() {
+        /*
+        xhr.readyState reference:
+
+        Value 	State               Description
+        0       UNSENT              Client has been created. open() not called yet.
+        1       OPENED              open() has been called.
+        2       HEADERS_RECEIVED    send() has been called, and headers and status are available.
+        3       LOADING             Downloading; responseText holds partial data.
+        4       DONE                The operation is complete.
+        */
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            this.setState({ip_address: response.ip});
+        }
+    }
     render() {
-        return <p>Nothing yet!</p>
+        return <p>{this.state.ip_address}</p>
     }
 }
 
