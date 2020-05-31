53c3a90a2a1f48d16bf80f48bddb970f1583157e
kan <erkan.dem@pm.me>
2020-05-31 14:40:17 +0200

    initialize application state in constructor

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,12 @@ import React, { Component } from "react"
 
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: {}
+        };
+    }
     render() {
         return (
             <div className="todoListMain">
